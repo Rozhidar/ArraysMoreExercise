30aec401fdeec1bcb72ef30d58ef63770c9f3cf8
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-07-24 02:50:37 +0200

    Added new project

---
+++ PascalTriangle/Program.cs
@@ -0,0 +1,19 @@
+﻿int rows = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < rows - i; j++)
+    {
+        Console.Write(" ");
+    }
+
+    int number = 1;
+
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write(number + " ");
+        number = number * (i - j) / (j + 1);
+    }
+
+    Console.WriteLine();
+}
